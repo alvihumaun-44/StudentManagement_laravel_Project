33e106a67d0b81ee214fc568b4ec0646a0d1a210
alvihumaun-44 <alvi.humaun44@gmail.com>
2024-02-11 22:04:08 +0600

    first commit

---
+++ resources/views/student/footer.blade.php
@@ -0,0 +1,59 @@
+ <!-- footer menu -->
+
+        <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="styles.css">
+  <title>Your Awesome Website</title>
+</head>
+<body>
+
+  <!-- Your website content goes here -->
+
+  <footer class="bg-dark text-light py-4">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <h3>Contact</h3>
+          <p>Email: <a href="https://www.alvi.humaun44@gmail.com" class="text-light">alvi.humaun44@gmail.com</a></p>
+          <p>Phone: +880 1401-205105</p>
+          <p>Location: Maheshpur,Jhenaidah</p>
+        </div>
+        
+        <div class="col-md-4">
+          <h3>Follow Us</h3>
+          <div class="social-icons">
+            <a href="https://web.facebook.com/desolate.alvi" target="blank" class="text-light mr-3"><i class="fab fa-facebook-f"></i>&nbsp;Facebook</a><br><br>
+            <a href="https://www.instagram.com/desolate_alvi/" target="blank" class="text-light"><i class="fab fa-instagram"></i>&nbsp;Instagram</a>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <h3>Quick Links</h3>
+          <ul class="list-unstyled">
+            <li><a href="#" class="text-light">Home</a></li>
+            <li><a href="#" class="text-light">Services</a></li>
+            <li><a href="#" class="text-light">Portfolio</a></li>
+            <li><a href="https://web.facebook.com/desolate.alvi" class="text-light">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <div class="footer-bottom text-center py-2">
+      <p class="mb-0">&copy;Student Managemanet System {{date('Y')}}</p>
+    </div>
+</footer>
+
+</body>
+</html>
+
+
+
+
+
+
+
